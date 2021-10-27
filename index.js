3ea225ef055ee477d33061c9e8014aadf8f16eac
Sandip Tamang <sandip.tamang@student.griffith.ie>
2021-10-27 18:29:51 +0100

    added onClick event handler on both components

--- index.js
+++ index.js
@@ -6,6 +6,47 @@ class App extends React.Component{
             breakLength:5,
             sessionLength:25
         }
+
+        this.decrementBreakLength = this.decrementBreakLength.bind(this);
+        this.incrementBreakLength = this.incrementBreakLength.bind(this);
+        this.decrementSessionLength = this.decrementSessionLength.bind(this);
+        this.incrementSessionLength = this.incrementSessionLength.bind(this);
+    }
+
+    decrementBreakLength(){
+        if(this.state.breakLength<=1){
+            return;
+        }
+        this.setState(state=>({
+            breakLength:state.breakLength - 1
+        }))
+    }
+
+    incrementBreakLength(){
+        if(this.state.breakLength>=60){
+            return;
+        }
+        this.setState(state=>({
+            breakLength:state.breakLength + 1
+        }))
+    }
+
+    decrementSessionLength(){
+        if(this.state.sessionLength<=1){
+            return;
+        }
+        this.setState(state=>({
+            sessionLength: state.sessionLength - 1
+        }))
+    }
+
+    incrementSessionLength(){
+        if(this.state.sessionLength>=60){
+            return;
+        }
+        this.setState(state=>({
+            sessionLength: state.sessionLength + 1
+        }))
     }
 
     render(){
@@ -15,8 +56,8 @@ class App extends React.Component{
                     <h1>25+5 Clock</h1>
                 </header>
                 <div className="break-session">
-                    <BreakLength breakLength={this.state.breakLength}/>
-                    <SessionLength sessionLength={this.state.sessionLength}/>
+                    <BreakLength breakLength={this.state.breakLength} decrementBreakLength={this.decrementBreakLength} incrementBreakLength={this.incrementBreakLength}/>
+                    <SessionLength sessionLength={this.state.sessionLength} decrementSessionLength={this.decrementSessionLength} incrementSessionLength={this.incrementSessionLength}/>
                 </div>
                 <div id="timer-label">
 
@@ -29,11 +70,11 @@ class App extends React.Component{
 function BreakLength(props){
     return(
         <div id="break-label">
-            <h1>Break Length</h1>
+            <h2>Break Length</h2>
             <div id="break-section">
-                <button id="break-decrement" className="btn btn-primary">-</button>
+                <button onClick={props.decrementBreakLength} id="break-decrement" className="btn btn-primary">-</button>
                 <h3 id="break-length">{props.breakLength}</h3>
-                <button id="break-increment" className="btn btn-primary">+</button>
+                <button onClick={props.incrementBreakLength} id="break-increment" className="btn btn-primary">+</button>
             </div>
         </div>
     );
@@ -42,11 +83,11 @@ function BreakLength(props){
 function SessionLength(props){
     return(
         <div id="session-label">
-            <h1>Session Length</h1>
+            <h2>Session Length</h2>
             <div id="session-section">
-                <button id="session-decrement" className="btn btn-primary">-</button>
+                <button onClick={props.decrementSessionLength} id="session-decrement" className="btn btn-primary">-</button>
                 <h3 id="session-length">{props.sessionLength}</h3>
-                <button id="session-increment" className="btn btn-primary">+</button>
+                <button onClick={props.incrementSessionLength} id="session-increment" className="btn btn-primary">+</button>
             </div>
         </div>
     );

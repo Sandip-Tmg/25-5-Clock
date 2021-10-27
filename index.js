ce5c2170abbaf84e5fad9825daa0c5614008682b
Sandip Tamang <sandip.tamang@student.griffith.ie>
2021-10-27 20:19:32 +0100

    added all the functions need for the app

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ class App extends React.Component{
 
         this.state={
             breakLength:5,
-            sessionLength:25
+            sessionLength:25,
+            timerMinute:25,
+            timerSecond:0,
+            intervalID:0
         }
 
         this.decrementBreakLength = this.decrementBreakLength.bind(this);
         this.incrementBreakLength = this.incrementBreakLength.bind(this);
         this.decrementSessionLength = this.decrementSessionLength.bind(this);
         this.incrementSessionLength = this.incrementSessionLength.bind(this);
+        this.playTimer = this.playTimer.bind(this);
+        this.startTimer = this.startTimer.bind(this);
+        this.stopTimer = this.stopTimer.bind(this);
+        this.reset = this.reset.bind(this);
+
     }
 
     decrementBreakLength(){
@@ -36,7 +44,8 @@ class App extends React.Component{
             return;
         }
         this.setState(state=>({
-            sessionLength: state.sessionLength - 1
+            sessionLength: state.sessionLength - 1,
+            timerMinute:state.sessionLength - 1
         }))
     }
 
@@ -45,8 +54,55 @@ class App extends React.Component{
             return;
         }
         this.setState(state=>({
-            sessionLength: state.sessionLength + 1
+            sessionLength: state.sessionLength + 1,
+            timerMinute:state.sessionLength + 1
         }))
+    }
+
+    
+    startTimer(){
+       let interval = setInterval(this.playTimer,1000);
+       console.log("Interval: "+interval);
+       this.setState({
+           intervalID:interval
+       })
+       console.log("State Interval: "+ this.state.intervalID)
+    }
+
+    playTimer(){
+        switch(this.state.timerSecond){
+            case 0:
+                this.setState(prevState=>({
+                    timerSecond:59,
+                    timerMinute:prevState.timerMinute-1
+                }))
+                break;
+            default:
+                this.setState(prevState=>({
+                    timerSecond:prevState.timerSecond-1
+                }))
+                break;
+        }
+
+        // the break length starts when minute and second finishes
+        if(this.state.timerMinute==0 && this.state.timerSecond==0){
+            this.setState(state=>({
+                timerMinute:state.breakLength,
+                timerSecond:state.timerSecond
+            }))
+        }
+    }
+
+    stopTimer(){
+        clearInterval(this.state.intervalID)
+    }
+
+    reset(){
+        this.stopTimer();
+        this.setState({
+            timerSecond:0,
+            timerMinute:this.state.sessionLength
+        })
     }
 
     render(){
@@ -58,9 +114,7 @@ class App extends React.Component{
                 <div className="break-session">
                     <BreakLength breakLength={this.state.breakLength} decrementBreakLength={this.decrementBreakLength} incrementBreakLength={this.incrementBreakLength}/>
                     <SessionLength sessionLength={this.state.sessionLength} decrementSessionLength={this.decrementSessionLength} incrementSessionLength={this.incrementSessionLength}/>
-                </div>
-                <div id="timer-label">
-
+                    <Session timerMinute={this.state.timerMinute} timerSecond={this.state.timerSecond} startTimer={this.startTimer} reset={this.reset} stopTimer={this.stopTimer}/>
                 </div>
             </div>
         )
@@ -91,6 +145,27 @@ function SessionLength(props){
             </div>
         </div>
     );
+}
+
+function Session(props){
+
+    return(
+        <div id="timer-label">
+            <h2>Session</h2>
+            <div id="time-left">
+                <span>{props.timerMinute}</span>
+                <span>:</span>
+                <span>{props.timerSecond<10 ?"0"+props.timerSecond:props.timerSecond}</span>
+            </div>
+            <div id="buttons">
+                <button id="start_stop" className="btn btn-primary" onClick={props.startTimer}>Start</button>
+                <button id="stop" className="btn btn-success" onClick={props.stopTimer}>Stop</button>
+                <button id="reset" className="btn btn-danger" onClick={props.reset}>Reset</button>
+            </div>
+        </div>
+    );
+    
+
 }
 
 ReactDOM.render(<App/>,document.getElementById('root'));
